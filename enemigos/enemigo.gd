e496c2d5972c58f872d044c21eba643157ce461f
FerCardosMartegani <103231341+FerCardosMartegani@users.noreply.github.com>
2026-08-23 01:15:44 -0300

    Enemigo PathFollower

---
+++ enemigos/enemigo.gd
@@ -0,0 +1,49 @@
+extends PathFollow2D
+
+var estado: String
+var mirando: String
+
+var ang: float
+var pAng: float
+
+@export var velocidad: float
+var pPosition: Vector2
+
+var sprites: AnimatedSprite2D
+var animar: String
+
+func _ready():
+	progress = 0
+	estado = "AVANZAR"
+	pPosition = position
+	
+	sprites = $AnimatedSprite2D
+
+func _process(delta):
+	if estado == "AVANZAR":
+		progress += velocidad
+		
+		if progress_ratio == 1:
+			queue_free()
+	
+	ang = pPosition.angle_to_point(position)
+	if ang != pAng:
+		var a45 = 0.25*PI
+		if ang > -a45*3 && ang < -a45:
+			mirando = "ARRIBA"
+		elif ang >= -a45 && ang <= a45:
+			mirando = "DERECHA"
+		elif ang > a45 && ang < a45*3:
+			mirando = "ABAJO"
+		elif ang >= a45*3 || ang <= -a45*3:
+			mirando = "IZQUIERDA"
+		print(str(ang) + ", " + mirando)
+		
+		animar = estado+"_"+mirando
+		if animar != sprites.animation:
+			sprites.play(animar)
+	
+	
+	pPosition = position
+	pAng = ang
+	
